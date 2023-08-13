74e971f4f4ecf5e2f4987ca2d583b4823dcac3b3
paul-daniel <dantchatchouang@gmail.com>
2023-08-13 16:21:51 +0200

    reset index

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,3 @@
 import { camelCase } from 'lodash';
 
 console.log(camelCase('hello world'));
-
-console.log('this was actually really hard to put in place u know. BuT i did it!');
-
-if (true) {
-  console.log('i am handsome');
-}
